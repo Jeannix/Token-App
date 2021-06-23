dd27a7a2b7d565bb213d09ad045f678e72185956
Rafael Cano <rafael.cano@ucsp.edu.pe>
2021-06-23 14:58:38 -0500

    Contribution test

--- test/main.py
+++ test/main.py
@@ -1,5 +1,6 @@
-	import sys
-	print("Python version")
-	print (sys.version)
-	print("Version info.")
-	print (sys.version_info)
+import sys
+#Probando a ver si puedo contribuir uwu
+print("Python version")
+print (sys.version)
+print("Version info.")
+print (sys.version_info)
